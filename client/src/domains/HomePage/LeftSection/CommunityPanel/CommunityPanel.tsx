d324aadbe67d9b1268cc456bd2e48660a4032a05
unknown <cuongjkne@gmail.com>
2021-09-18 15:06:18 +0700

    add LeftSection components

---
+++ client/src/domains/HomePage/LeftSection/CommunityPanel/CommunityPanel.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+//icons
+import PlusIcon from '../../../../common/Icon/PlusIcon';
+//styles
+import { Container, Section } from './CommunityPanel.style';
+
+function CommunityPanel() {
+  return (
+    <Container>
+      <Section>
+        <div>
+          <Link to="/">Groups</Link>
+          <Link to="/">Events</Link>
+          <Link to="/">Followed Hastags</Link>
+        </div>
+        <div>
+          <PlusIcon />
+        </div>
+      </Section>
+      <Link to="">Discover more</Link>
+    </Container>
+  );
+}
+
+export default CommunityPanel;
---
+++ client/src/domains/HomePage/LeftSection/PersonalPanel/PersonalPanel.tsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+//icons
+import ConnectIcon from '../../../../common/Icon/ConnectIcon';
+import MyItemsIcon from '../../../../common/Icon/MyItemsIcon';
+import PremiumIcon from '../../../../common/Icon/PremiumIcon';
+//styles
+import { Container, Header, ConnectionSection, PremiumSection, MyItems } from './PersonalPanel.style';
+
+function PersonalPanel() {
+  return (
+    <Container>
+      <Header>
+        <img src="/images/background-user.svg" alt="background-user.svg" />
+
+        <div>
+          <Link to="">
+            <img src="/images/user.jpg" alt="user.jpg" />
+            Cường Nguyễn
+          </Link>
+          <p>Front-end Web Developer</p>
+        </div>
+      </Header>
+      <ConnectionSection to="/">
+        <div>
+          <p>
+            <span>Connections</span> <br /> Grow your network
+          </p>
+          <ConnectIcon />
+        </div>
+      </ConnectionSection>
+      <PremiumSection to="/">
+        <p>Access exclusive tools & insights</p>
+        <p>
+          <PremiumIcon />
+          Try Premium for free
+        </p>
+      </PremiumSection>
+      <MyItems to="/">
+        <MyItemsIcon />
+        <span>My items</span>
+      </MyItems>
+    </Container>
+  );
+}
+
+export default PersonalPanel;
