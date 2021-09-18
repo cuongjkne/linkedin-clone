978ff3067ef6affe8ac4e424162d6afbe14f2cfb
unknown <cuongjkne@gmail.com>
2021-09-18 15:11:08 +0700

    add MiddleSection components

---
+++ client/src/domains/HomePage/MiddleSection/MiddleSection.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+function MiddleSection() {
+  return <Container>MiddleSection</Container>;
+}
+const Container = styled.div``;
+export default MiddleSection;
+//TODO: middle
